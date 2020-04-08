889c5877f163918107c68b3b0254b788ca45780e
Mikołaj <mikgrzebieluch@gmail.com>
2020-04-08 19:09:38 +0200

    Added Choose Role Window

--- client/__init__.py
+++ client/__init__.py
@@ -2,5 +2,8 @@ import server
 from . import gui
 from . import networker
 from . import chatManager
+
+roles = ['Player', 'Master']
+
 def start():
-    pass
+	pass
--- client/gui/__init__.py
+++ client/gui/__init__.py
@@ -1,2 +1,35 @@
+from PyQt5 import QtWidgets
+import client
+
+app = QtWidgets.QApplication([])
+
+class ChooseRoleWindow(QtWidgets.QDialog):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.choosen_role = ""
+
+		layout = QtWidgets.QHBoxLayout()
+
+		for role in client.roles:
+			button = QtWidgets.QPushButton()
+			button.setText(role)
+			button.clicked.connect(self.setRole(role))
+			layout.addWidget(button)
+
+		self.setLayout(layout)
+
+	def setRole(self, role):
+		def _setRole():
+			self.choosen_role = role
+			self.close()
+		return _setRole
+
+	def choose(self):
+		self.exec_()
+		return self.choosen_role
+
+
 class GUI:
-    pass
+	def chooseRole(self):
+		dialog_window = ChooseRoleWindow()
+		return dialog_window.choose()
