0cac83374df64f1f5aab56c4038f3cdffa0d8912
MrHackerBonzo <barcis2@staszic.waw.pl>
2020-04-08 16:51:34 +0200

    Some advices what to change

--- client/networker/__init__.py
+++ client/networker/__init__.py
@@ -1,16 +1,16 @@
 # Client side Networker
-
 import socket
 import random
+import settings
 
 
 class Networker:
     """ Class responsible for serving networking. """
 
-    def __init__(self):
+    def __init__(self): # Zastrzeżeie: zmień __init__ na start i dodaj funkcje setGUI i setChatManager
         self.client_id = random.randint(1, 10000000000000)
         print(self.client_id)
-        self.connector = Connection("localhost", 2134, self.client_id, "Janek")
+        self.connector = Connection("localhost", settings.port, self.client_id, "Janek")  # Zastrzeżenie: zmień z "localhost" na argument który będzie kluczem od serwera; tak samo z imieniem
 
 
 
@@ -23,12 +23,12 @@ class Connection:
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client_id = client_id
         self.sock.connect((self.server_adress, self.port))
-        self.send("@register@")
-        self.send("@Name_change@" + player_name)
+        self.send("@register@")                        # Zastrzeżenie: koniecznie zmień to na jsona. Budowanie własnego protokołu będzie się później baaaardzo syfiło, a nie kosztuje cię to jakoś dużo
+        self.send("@Name_change@" + player_name)       # Zastrzeżenie: to samo tutaj
 
     def send(self, data):
-        self.sock.sendall(bytes("§" + str(self.client_id) + "§" + str(data), "utf-8"))
-        print("Sent:     {}".format(data))
+        self.sock.sendall(bytes("§" + str(self.client_id) + "§" + str(data), "utf-8")) # Ponownie tutaj; kolejne wiadomości oddzielaj enterem raczej
+        print("Sent:     {}".format(data))                                             # Zmień z stdio na stderr raczej
 
     def disconnect(self):
         self.sock.close()
