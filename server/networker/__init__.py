535230e138873b7b7bb2eb7d102ba67c8fc42849
pggPL <pawelgadzinski@gmail.com>
2020-04-09 14:00:47 +0200

    Ip address downloading without connecting to webpages. Added json sending between client and server. Added serving player initialization.

--- server/networker/__init__.py
+++ server/networker/__init__.py
@@ -3,23 +3,49 @@ import settings
 import socketserver
 import socket
 import threading
+import json
 import urllib.request
+
+stack_of_commands = []
+stack_of_commands_th = threading.Thread()
 
 
 class Networker:
     """This class supports communication between Game and client"""
+
     def __init__(self):
+        self.local = False
+        self.IP = '127.0.0.1'
         self.server = GameServer()
-        ip = urllib.request.urlopen('http://ifconfig.me/ip').read() # zastrzeżenie: Spróbuj poszperać czy nie da się zrobić tego lokalnie z routera
-        ip.decode("utf-8")
+        self.get_ip()
         self.server.serverStart('localhost', settings.port)
-        self.player_names = {}
+        self.players = []
+        ServerHandler.command_handler = self
+
+    def get_ip(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            # doesn't even have to be reachable
+            s.connect(('10.255.255.255', 1))
+            self.IP = s.getsockname()[0]
+        except:
+            self.IP = '127.0.0.1'
+        finally:
+            s.close()
+
+    def command(self, command):
+        if command['type'] == 'initialization':
+            self.player_initialize(command)
+
+    def player_initialize(self, command):
+        self.players.append(NetworkPlayer(command['IP'], command['player_name'], command['client_token']))
+        print("Player added. Name: {0}. IP: {1}. Token: {2}.".format(command['IP'], command['player_name'], command['client_token']))
 
     def getAccesKey(self) -> bytes:
-        return self.server.host                                     # zastrzeżenie: dodaj parametr local=False i zwracaj defaultowo to co ma być zwrócone dla klientów spoza sieci
+        return self.server.host  # zastrzeżenie: dodaj parametr local=False i zwracaj defaultowo to co ma być zwrócone dla klientów spoza sieci
 
 
-class GameServer:                                                   # zastrzeżenie: nie jestem pewien czy ta klasa wogóle jest potrzebna, czy nie można tego wszystkiego zrobić w Networkerze, no ale jaki jest twój zamysł to nwm więc tylko sugeruję tutaj
+class GameServer:  # zastrzeżenie: nie jestem pewien czy ta klasa w ogóle jest potrzebna, czy nie można tego wszystkiego zrobić w Networkerze, no ale jaki jest twój zamysł to nwm więc tylko sugeruję tutaj
     """ Server class, server connection to clients """
 
     def __init__(self):
@@ -52,20 +78,19 @@ class ServerHandler(socketserver.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).strip()
         data_string = self.data.decode("UTF-8")
-        data_split = data_string.split("§")        # Zastrzeżenie: zamień separator na entery raczej
-        typ = 0                                    # Zastrzeżenie: zamień na True/False
-        player = 0
-        message = ""
-        # Zastrzeżenie:                      jak już wcześniej mówiłem, zrobienie tego jsonem i dictami będzie ładniejsze i czytelniejsze
-        for segment in data_split[1:]:
-            if typ == 0:                           # Zastrzeżenie: zamień na True/False
-                player = segment
-            else:
-                message = segment
-                print("{0} send a message {1}. \n".format(player, message))
-
-            typ = 1 - typ                          # Zastrzeżenie: zamień na True/False
+        data_split = data_string.split("§")
+        del data_split[-1]
+        for command in data_split:
+            #tutaj trzeba będzie wywołać funkcję uruchamiającą komendy z Networkera
+            self.command_handler.command(json.loads(command))
 
 
 
-        #self.request.sendall(self.data.upper())
+
+
+class NetworkPlayer:
+    """ Class which represents player connected to server."""
+    def __init__(self, IP, name, client_token):
+        self.IP = IP
+        self.name = name
+        self.client_token = client_token
