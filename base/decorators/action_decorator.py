e1a3470e3036b3ca58f8c9a95731e91c790f8af7
Mikołaj <mikgrzebieluch@gmail.com>
2020-04-10 18:01:15 +0200

    Advice for wrappers

--- base/decorators/action_decorator.py
+++ base/decorators/action_decorator.py
@@ -1,5 +1,9 @@
 from threading import Thread
+from functools import wraps
 
 def action(func):
+	# polecam uzywac "@wraps", poniewaz wtedy nazwa funkcji 
+	# i inne jej atrybuty pozostana bez zmian
+	@wraps(func)
     def action_wrapper(self,*args,**kwargs):
         self.
--- base/decorators/thread_decorator.py
+++ base/decorators/thread_decorator.py
@@ -1,6 +1,10 @@
 from threading import Thread
+from functools import wraps
 
 def async(func):
+	# polecam uzywac "@wraps", poniewaz wtedy nazwa funkcji 
+	# i inne jej atrybuty pozostana bez zmian
+	@wraps(func)
     def async_wrapper(*args,**kwargs)
         t=Thread(target=func,args=args,kwargs=kwargs)
         t.start()
