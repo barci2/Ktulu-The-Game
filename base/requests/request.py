7a555fce4bfa546c82ad8f4688154b0f3767219a
MrHackerBonzo <barcis2@staszic.waw.pl>
2020-04-15 09:35:37 +0200

    Bug fixes to Request Class

--- base/requests/request.py
+++ base/requests/request.py
@@ -24,23 +24,21 @@ class Request():
 
         if len(args)+len(kwargs)==2:
             self._load(*args,**kwargs)
-        elif len(args)==1 and len(kwargs)==0:
-            self._initResponse(args[0])
-        elif len(args)==0 and len(kwargs)==1:
-            self._initResponse(list(kwargs.values())[0])
+        elif len(args)+len(kwargs)==1:
+            self._initResponse(*args,**kwargs)
         elif len(args)+len(kwargs)==0:
-            self._init_new(networker)
+            self._initNew()
         else:
             raise AssertionError("__init__ arguments invalid")
 
-    def _init_new(self):
+    def _initNew(self):
         self._id=random.randint(0,2**(id_length*8)).to_bytes(id_length,"big")
 
     def _initResponse(self,response):
         self._id=response.id()
         self._player=response.player()
 
-    def _load(self,networker,message,player):
+    def _load(self,message,player):
         try:
             obj=pickle.loads(message)
         except:
