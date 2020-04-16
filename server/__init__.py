d96f9c6825ccc7324262b5198a185168be527fcb
MrHackerBonzo <barcis2@staszic.waw.pl>
2020-04-16 12:35:42 +0200

    fixing variable naming scheme

--- server/__init__.py
+++ server/__init__.py
@@ -12,25 +12,37 @@ from base.decorators import toThread
 
 # Constructing main classes
 _networker=networker.Networker()
-_chatManager=chatManager.ChatManager()
-_gameKernel=gameKernel.GameKernel()
+_chat_manager=chatManager.ChatManager()
+_game_kernel=gameKernel.GameKernel()
 
 # Sharing classes between themselves
-#_networker.setChatManager(_chatManager)
-#_networker.setGameKernel(_gameKernel)
+#_networker.setChatManager(_chat_manager)
+#_networker.setGameKernel(_game_kernel)
 
-#_chatManager.setGameKernel(_gameKernel)
-#_chatManager.setNetworker(_networker)
+#_chat_manager.setGameKernel(_game_kernel)
+#_chat_manager.setNetworker(_networker)
 
-#_gameKernel.setNetworker(_networker)
-#_gameKernel.setChatManager(_chatManager)
+#_game_kernel.setNetworker(_networker)
+#_game_kernel.setChatManager(_chat_manager)
 
 #############
 # Functions #
 #############
 @toThread
 def start() -> bytes:
-    global _networker,_chatManager,_gameKernel
+    global _networker,_chat_manager,_game_kernel
     _networker.start()
-    _chatManager.start()
-    _gameKernel.start()
+    _chat_manager.start()
+    _game_kernel.start()
+
+def getNetworker():
+    global _networker
+    return _networker
+
+def getChatManager():
+    global _chat_manager
+    return _chat_manager
+
+def getGameKernel():
+    global _game_kernel
+    return _game_kernel
